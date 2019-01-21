7e05acb8165ccabd4d167e0c1d0148ada6910a6e
tati-micheletti <tati.micheletti@gmail.com>
2019-01-21 15:39:28 -0800

    updated defineStudyArea() and fixed minor path bug

--- functions/defineStudyArea.R
+++ functions/defineStudyArea.R
@@ -16,6 +16,9 @@
 #'                          in the BCR6 or 'Canada' if the whole Canadian BCR6 is wanted.
 #'                          Default is `NULL`.
 #' @param destinationFolder Path to where to save downloaded files. Default is `tempdir()`.
+#' 
+#' @param ...               Arguments to be passed to `prepInputs` or `Cache` (i.e. targetFile, 
+#'                          cacheId, destinationPath, overwrite, etc.).
 #'
 #' @author Tati Micheletti and Geneviève Degré-Timmons
 #' @export
@@ -27,12 +30,13 @@
 #' rp <- defineStudyArea(testArea = TRUE, specificTestArea = "BCR6", mapSubset = NULL) ## rp is the whole Boreal Taiga plains Bird conservation regions (BCR6)
 #' rp <- defineStudyArea(testArea = TRUE, specificTestArea = "Northwest Territories", mapSubset = NULL) ## Northwest Territories
 #' rp <- defineStudyArea(testArea = TRUE, specificTestArea = "BCR6", mapSubset = "Northwest Territories") ## Nortwest Territories inside BCR6
-#' 
 
 library(SpaDES.tools)
 library(reproducible)
 
-defineStudyArea <- function(testArea = NULL, specificTestArea = NULL, mapSubset = NULL, destinationFolder = tempdir()) {
+defineStudyArea <- function(testArea = NULL, specificTestArea = NULL, mapSubset = NULL, ...) {
+  dots <- list(...)
+  dots$destinationPath <- if (is.null(dots$destinationPath)) tempdir()
   rP <- NULL
   if (any(is.null(testArea), (!is.null(testArea) &
                               testArea == FALSE))) {
@@ -54,15 +58,14 @@ defineStudyArea <- function(testArea = NULL, specificTestArea = NULL, mapSubset 
       if (specificTestArea == "BCR6") {
         if (is.null(mapSubset)) {
           message(crayon::yellow("Test area is TRUE, specificTestArea is 'BCR6', and mapSubset is NULL. Cropping and masking to the whole BCR6."))
-          rP <- reproducible::prepInputs(url = "https://drive.google.com/open?id=187R47guUko6p8WY-wCAlz_lPHKaGguGX",
-                           destinationPath = destinationFolder
+          rP <- reproducible::prepInputs(url = "https://drive.google.com/open?id=1sEiXKnAOCi-f1BF7b4kTg-6zFlGr0YOH",
+                           destinationPath = dots$destinationPath, ...
           )
         }
         if (!is.null(mapSubset) && mapSubset != "Canada") {
           sA <- reproducible::prepInputs(url = "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gpr_000b11a_e.zip",
-                           targetFile = "gpr_000b11a_e.shp",
-                           alsoExtract = "similar",
-                           destinationPath = destinationFolder) %>%
+                           destinationPath = dots$destinationPath, 
+                           ...) %>%
             raster::subset(PRENAME %in% mapSubset)
           if (nrow(sA@data) == 0) {
             stop(paste0("There is no Canadian Province called ",
@@ -70,26 +73,22 @@ defineStudyArea <- function(testArea = NULL, specificTestArea = NULL, mapSubset 
                         ". Please provide a Canadian province name in English for subsetMap, ",
                         "or use 'NULL' (does not subset BCR6, dangerous when dealing with higher resolution)."))
           }
-          rP <- reproducible::prepInputs(url = "https://drive.google.com/open?id=187R47guUko6p8WY-wCAlz_lPHKaGguGX", 
-                           alsoExtract = "similar",
-                           studyArea = sA,
-                           destinationPath = destinationFolder)
+          rP <- reproducible::prepInputs(url = "https://drive.google.com/open?id=1sEiXKnAOCi-f1BF7b4kTg-6zFlGr0YOH", 
+                           studyArea = sA, destinationPath = dots$destinationPath, ...)
           return(rP)
         } else {
         }
         if (!is.null(mapSubset) && mapSubset == "Canada") {
           message(crayon::yellow("Test area is TRUE. Cropping and masking to the Canadian BCR6."))
           rP <- reproducible::prepInputs(url = "https://drive.google.com/open?id=187R47guUko6p8WY-wCAlz_lPHKaGguGX", 
-                           alsoExtract = "similar",
-                           destinationPath = destinationFolder)
+                           destinationPath = dots$destinationPath, ...)
           return(rP)
         }
       } else {
         if (!is.null(specificTestArea)) {
           rP <- reproducible::prepInputs(url = "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gpr_000b11a_e.zip",
-                           targetFile = "gpr_000b11a_e.shp",
-                           alsoExtract = "similar",
-                           destinationPath = destinationFolder) %>%
+                           destinationPath = dots$destinationPath, 
+                          ...) %>%
             raster::subset(PRENAME == specificTestArea)
           if (nrow(rP@data) == 0) {
             stop(paste0("There is no Canadian Province called ",

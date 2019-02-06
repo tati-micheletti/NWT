670d0e4502c39b254acd44d8875ac70cee6d1238
Tati Micheletti <tati.micheletti@gmail.com>
2019-02-06 22:45:01 +0000

    updated birdsNWT and runMe.R

--- modules/birdsNWT/R/loadStaticLayers.R
+++ modules/birdsNWT/R/loadStaticLayers.R
@@ -1,6 +1,8 @@
 loadStaticLayers <- function(folderUrl = extractURL("urlStaticLayers"),
                              pathData = dataPath(sim),
                              cloudFolderID = sim$cloudFolderID){
-  stk <- cloudCache(prepInputs, url = folderUrl, destinationPath = pathData)
+
   browser()
+  stk <- cloudCache(prepInputs, url = folderUrl, destinationPath = pathData,
+                    cloudFolderID = cloudFolderID)
 }

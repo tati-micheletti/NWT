6345d4850d8c7ee5e53ac47baefbd9a60d75ce93
Tati Micheletti <tati.micheletti@gmail.com>
2019-02-08 23:35:52 +0000

    updated birdNWT module

--- modules/birdsNWT/R/loadStaticLayers.R
+++ modules/birdsNWT/R/loadStaticLayers.R
@@ -1,9 +1,18 @@
 loadStaticLayers <- function(fileURL = extractURL("urlStaticLayers"),
                              pathData = dataPath(sim),
-                             cloudFolderID = sim$cloudFolderID){
-
-  browser()
-  stk <- prepInputs(url = fileURL, targetFile = "bcr6_2011rasters250.grd",
-                    destinationPath = pathData, archive = "bcr6_2011rasters250.zip",
-                    cloudFolderID = cloudFolderID, fun = "raster::stack")
+                             cloudFolderID = sim$cloudFolderID,
+                             studyArea = sim$studyArea){
+  require("raster")
+  stkPre <- preProcess(url = fileURL, targetFile = "bcr6_2011rasters250.grd",
+                    destinationPath = pathData)
+  stk <- raster::stack(stkPre$targetFilePath)
+  stkNames <- unlist(lapply(X = 1:length(stk@layers), FUN = function(layers){
+    lay <- stk@layers[[layers]]@data@names
+    return(lay)
+  }))
+  stk <- postProcess(stk, studyArea = studyArea)
+  names(stk) <- stkNames
+  fixedLayers <- stkNames[!grepl(pattern = "Species", x = stkNames)]
+  staticLayers <- raster::subset(x = stk, subset = fixedLayers)
+  return(staticLayers)
 }

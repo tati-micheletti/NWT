fa926638bfc60663a5a12f94980506501fbefe71
tati-micheletti <tati.micheletti@gmail.com>
2019-02-21 13:56:09 -0800

    added omitArgs for cloudCache birdModels

--- modules/birdsNWT/R/loadBirdModels.R
+++ modules/birdsNWT/R/loadBirdModels.R
@@ -4,13 +4,15 @@ loadBirdModels <- function(birdsList = sim$birdsList,
                            pathData = dataPath(sim)){
   reproducible::Require("googledrive")
   filesToDownload <- cloudCache(googledrive::drive_ls, path = folderUrl, pattern = "brt1.R",
-                                cloudFolderID = cloudFolderID, useCloud = TRUE)
+                                cloudFolderID = cloudFolderID, useCloud = TRUE,
+                                omitArgs = c("destinationPath", "cloudFolderID"))
   modelsPath <- checkPath(file.path(pathData, "models"), create = TRUE)
   modelsForBirdList <- filesToDownload$name[grepl(pattern = paste(birdsList, collapse = "|"), x = filesToDownload$name)]
   downloadedModels <- lapply(X = modelsForBirdList, FUN = function(modelFile){
     if (!file.exists(file.path(modelsPath, modelFile))){
       cloudCache(googledrive::drive_download, file = paste0(folderUrl, modelFile),
-                 path = file.path(modelsPath, modelFile), cloudFolderID = cloudFolderID)
+                 path = file.path(modelsPath, modelFile), cloudFolderID = cloudFolderID,
+                 omitArgs = c("destinationPath", "cloudFolderID"))
     }
     return(get(load(file.path(modelsPath, modelFile))))
   })

10c1771a38e5be342b8d636197f2fd74ec2af684
tati-micheletti <tati.micheletti@gmail.com>
2019-02-22 06:39:25 -0800

    updated bird module and fixed bug in passing url

--- modules/birdsNWT/R/loadBirdModels.R
+++ modules/birdsNWT/R/loadBirdModels.R
@@ -3,16 +3,21 @@ loadBirdModels <- function(birdsList = sim$birdsList,
                            cloudFolderID = sim$cloudFolderID,
                            pathData = dataPath(sim)){
   reproducible::Require("googledrive")
-  filesToDownload <- cloudCache(googledrive::drive_ls, path = folderUrl, pattern = "brt1.R",
-                                cloudFolderID = cloudFolderID, useCloud = TRUE,
-                                omitArgs = c("destinationPath", "cloudFolderID"))
+  
+  # filesToDownload <- cloudCache(googledrive::drive_ls, path = folderUrl, pattern = "brt1.R",
+  #                               cloudFolderID = cloudFolderID, useCloud = TRUE,
+  #                               omitArgs = "cloudFolderID")
+  filesToDownload <- Cache(googledrive::drive_ls, path = as_id(folderUrl), pattern = "brt1.R")
   modelsPath <- checkPath(file.path(pathData, "models"), create = TRUE)
   modelsForBirdList <- filesToDownload$name[grepl(pattern = paste(birdsList, collapse = "|"), x = filesToDownload$name)]
   downloadedModels <- lapply(X = modelsForBirdList, FUN = function(modelFile){
     if (!file.exists(file.path(modelsPath, modelFile))){
-      cloudCache(googledrive::drive_download, file = paste0(folderUrl, modelFile),
-                 path = file.path(modelsPath, modelFile), cloudFolderID = cloudFolderID,
-                 omitArgs = c("destinationPath", "cloudFolderID"))
+      # cloudCache(googledrive::drive_download, file = paste0(folderUrl, modelFile),
+      #            path = file.path(modelsPath, modelFile), cloudFolderID = cloudFolderID,
+      #            omitArgs = c("path", "cloudFolderID"))
+      Cache(googledrive::drive_download, file = paste0(folderUrl, modelFile),
+                 path = as_id(file.path(modelsPath, modelFile)),
+                 omitArgs = "path")
     }
     return(get(load(file.path(modelsPath, modelFile))))
   })
--- modules/birdsNWT/birdsNWT.R
+++ modules/birdsNWT/birdsNWT.R
@@ -114,7 +114,7 @@ doEvent.birdsNWT = function(sim, eventTime, eventType) {
                                                                nCores = P(sim)$nCores,
                                                                # cloudFolderID = sim$cloudFolderID,
                                                                omitArgs = c("destinationPath", "nCores", 
-                                                                            "useParallel", "pathData")#,
+                                                                            "useParallel", "pathData"),
                                                                             # "cloudFolderID", 
                                                                             # "useCloud"),
                                                                # useCloud = TRUE,
@@ -130,7 +130,7 @@ doEvent.birdsNWT = function(sim, eventTime, eventType) {
 }
 
 .inputObjects <- function(sim) {
-  c
+  
   dPath <- asPath(getOption("reproducible.destinationPath", dataPath(sim)), 1)
   message(currentModule(sim), ": using dataPath '", dPath, "'.")
   if (!suppliedElsewhere(object = "birdsList", sim = sim)){
@@ -146,6 +146,5 @@ doEvent.birdsNWT = function(sim, eventTime, eventType) {
                        "BAOR", "HOWR", "WTSP", "CAWA", "RUBL", "AMRO", "HOLA", "AMRE", 
                        "AMGO", "AMCR", "ALFL")  
   }
-  
   return(invisible(sim))
 }

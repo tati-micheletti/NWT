745ce53b4cb1f827819fe8797c4c0ad26b592a44
Tati Micheletti <tati.micheletti@gmail.com>
2019-05-28 23:28:24 +0000

    added the module commits and updated

--- NWT_CS.R
+++ NWT_CS.R
@@ -26,31 +26,6 @@ if (updateSubmodules){
   update.packages(checkBuilt = TRUE)
 }
 
-# Commits on 28th May - Whole NWT run
-# Assumptions: 
-# FIRE: fireSense runs a "climate sensitive" fire from 2001 to 2011 using the 2011 climate layer
-# LANDR: Assumption at a landscape level is ok. We are assuming that you (we) are essentially making 
-# the assumption that 2011 looked like 2001. At the _landscape_ level that's not a terrible assumption
-# Oldest PSP stand is way younger than the data we have
-# 35335d977c5e432fbaa7301bdcc8112b5d4dfc80 modules/Biomass_regeneration (heads/development)
-# 019d688e1858792d3eddf3e9d1551a117d55535c modules/Boreal_LBMRDataPrep (heads/development)
-# 84886af3a99fd341c7461158c1ae8c25a8f0c099 modules/LBMR (heads/development)
-# a40bafbb4ea338f37ad8efdae06c2b32aad5f3af modules/LBMR2LCC_DataPrep (heads/master)
-# 0d674d3a264e59148aa03fa4f3d004a007d115d2 modules/LandR_BiomassGMOrig (heads/development)
-# 881957951841d667734556b84e39789622518343 modules/MDC_NWT_DataPrep (heads/master)
-# 6e140843c8b307b69128289b8e377072b9265676 modules/PSP_Clean (heads/master)
-# c120d8e1b106cd8d31e54f76bc07e8afbbf23309 modules/birdsNWT (heads/master)
-# 1d1d04d410d55eb07961476dd97073918bb1e63a modules/caribouPopGrowthModel (heads/master)
-# 7797b61b6291400fe3c709744ea367a33c072c3b modules/caribouRSF (heads/master)
-# 4fc2e61cabf063722b17bb645ce0a180a3331899 modules/climate_NWT_DataPrep (heads/master)
-# dba12450d340ef2a277cac00b633967745db0f80 modules/comm_metricsNWT (heads/master)
-# d976b680f99d7a029dc216984c5fa96c96df42c4 modules/fireSense_EscapePredict (heads/master)
-# 78ddd05efd17d9e9931201ee9449ba8dc99f18e7 modules/fireSense_FrequencyPredict (heads/master)
-# dfc61faa8272e9c06c2f4ebeeada58d298c874cc modules/fireSense_NWT (heads/master)
-# c3d3c54e82fbfb34ff53fe34fec9cc3ea8b0bec3 modules/fireSense_NWT_DataPrep (heads/master)
-# 886184b6d832aee3a57186b53dec65eea1ae16ed modules/gmcsDataPrep (heads/development)
-# 38557980e8675ce39aecafbd6fc6886a7a0cd57b modules/scfm (heads/development)
-
 # Source all common functions
 invisible(sapply(X = list.files(file.path(getwd(), "functions"), full.names = TRUE), FUN = source))
 

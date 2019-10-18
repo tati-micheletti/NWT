bc91fe1457d4ef6d2da50a96bbc7f139a3ed5434
Alex Chubaty <alex.chubaty@gmail.com>
2019-10-18 09:14:45 -0600

    Update modulesPlotScript.R
    minor formatting

--- modulesPlotScript.R
+++ modulesPlotScript.R
@@ -1,6 +1,8 @@
-# Modules Plot
-
+## Modules Plot
+library("data.table")
+library("ggplot2")
 library("reproducible")
+
 urlTable <- "https://drive.google.com/open?id=1D2Ivr7vJ_u8C_RambOiG8elRb29IjCE9"
 
 maxVersion <- 2
@@ -18,7 +20,6 @@ dt[, specificComponent  := factor(specificComponent, unique(dt$specificComponent
 
 data.table::setkey(dt, groupOrFamily)
 
-library("ggplot2")
 p <- ggplot(data = dt, mapping = aes(x = specificComponent, y = composedMilestone)) +
   geom_col(aes(fill = groupOrFamily), position = position_dodge2(reverse = TRUE)) +
   coord_flip() +
@@ -26,10 +27,10 @@ p <- ggplot(data = dt, mapping = aes(x = specificComponent, y = composedMileston
   scale_fill_discrete(drop = FALSE) +
   scale_y_discrete(drop=FALSE)
 
-
+# how is below diff from above?
 p <- ggplot(data = dt, mapping = aes(x = specificComponent, y = composedMilestone)) +
-geom_col(aes(fill = groupOrFamily), position = position_dodge2(reverse = TRUE)) +
-coord_flip() +
-scale_fill_discrete(drop=FALSE) +
-scale_y_discrete(drop=FALSE)
+  geom_col(aes(fill = groupOrFamily), position = position_dodge2(reverse = TRUE)) +
+  coord_flip() +
+  scale_fill_discrete(drop=FALSE) +
+  scale_y_discrete(drop=FALSE)
 p

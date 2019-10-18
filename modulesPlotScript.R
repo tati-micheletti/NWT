2e52ccd728085b4ed9be059dda0d4c4917349a3b
Tati Micheletti <tati.micheletti@gmail.com>
2019-10-18 04:38:55 +0000

    Script for the modules

---
+++ modulesPlotScript.R
@@ -0,0 +1,35 @@
+# Modules Plot
+
+library("reproducible")
+urlTable <- "https://drive.google.com/open?id=1D2Ivr7vJ_u8C_RambOiG8elRb29IjCE9"
+
+maxVersion <- 2
+dt <- reproducible::preProcess(url = urlTable, targetFile = "moduleStatusTable.csv", 
+                               destinationPath = file.path(getwd(), "figures/"))
+dt <- data.table::fread(dt$targetFilePath)
+# dt <- reproducible::prepInputs(url = urlTable, targetFile = "moduleStatusTable.csv", 
+#                        destinationPath = file.path(getwd(), "figures/"), fun = "data.table::fread")
+
+names(dt)[names(dt) == "colors"] <- "plotColors"
+composedMilestone <- paste0(rep(0:maxVersion, each = 6), c("D","P","T","R","V","M"))
+dt[, composedMilestone  := factor(paste0(version, milestone), composedMilestone)]
+dt[, groupOrFamily  := factor(groupOrFamily, unique(dt$groupOrFamily))]
+dt[, specificComponent  := factor(specificComponent, unique(dt$specificComponent))]
+
+data.table::setkey(dt, groupOrFamily)
+
+library("ggplot2")
+p <- ggplot(data = dt, mapping = aes(x = specificComponent, y = composedMilestone)) +
+  geom_col(aes(fill = groupOrFamily), position = position_dodge2(reverse = TRUE)) +
+  coord_flip() +
+  # scale_fill_manual(values = dt$plotColors) + 
+  scale_fill_discrete(drop = FALSE) +
+  scale_y_discrete(drop=FALSE)
+
+
+p <- ggplot(data = dt, mapping = aes(x = specificComponent, y = composedMilestone)) +
+geom_col(aes(fill = groupOrFamily), position = position_dodge2(reverse = TRUE)) +
+coord_flip() +
+scale_fill_discrete(drop=FALSE) +
+scale_y_discrete(drop=FALSE)
+p

7ab5ac3e6f2cc30cd1a4cc41b47e59d6d7f9c6c3
Tati Micheletti <tati.micheletti@gmail.com>
2019-11-02 00:50:50 +0000

    Removed funs from project and put in usefun

--- posthocFunctions/generatingFiguresAndAnimations.R
+++ posthocFunctions/generatingFiguresAndAnimations.R
@@ -29,8 +29,8 @@ invisible(lapply(paste0("/mnt/data/Micheletti/NWT/posthocFunctions/", c("plotMax
                                                               "totalBiomassPerSpecies.R",
                                                               "plotBurnSummary.R",
                                                               "disturbancePlotCaribou.R")), source))
-# 
-CSfolder <- "outputs/09OCT19/LandR.CS_fS/run1"
+
+CSfolder <- "outputs/23OCT19/LandR.CS_fS/run1"
 typeSim <- "CS"
 
 # biomassPerSpecies_1 <- totalBiomassPerSpecies(folderData = paste0(CSfolder, "1"), typeSim = paste0(typeSim, "1"))
@@ -50,7 +50,7 @@ biomassPerSpecies <- totalBiomassPerSpecies(folderData = CSfolder, years = c(seq
 biomassPerSpeciesProp <- totalBiomassPerSpecies(folderData = CSfolder, years = c(seq(2001, 2091, by = 10), 2100),
                                                     typeSim = typeSim, proportional = TRUE)
 burnSumm <- plotBurnSummary(CSfolder, typeSim = typeSim, lastYear = 2100) # theObject = LandR.CS_fS$burnSummary # if we have the object
-disturbPlot <- disturbancePlotCaribou(CSfolder, typeSim = typeSim)
+disturbPlot <- forestAgePlot(CSfolder, typeSim = typeSim)
 
 # If burnsummary is not available, only if we have burnDT...
 # createBurnSummary(folderData = CSfolder, typeSim = typeSim)
@@ -58,7 +58,8 @@ disturbPlot <- disturbancePlotCaribou(CSfolder, typeSim = typeSim)
 # source('/mnt/data/Micheletti/NWT/posthocFunctions/makeAllPlots.R') # NEVER RUN ALL REPETITIONS AT ONCE!
 # run7 <- makeAllPlots(CSfolder = "30JUL19/run7", typeSim = "CS_run7")
 
-# ~~~~~~~~~~~~~~~~~~~~ COMPARISON EDEhzhie
+# ~~~~~~~~~~~~~~~~~~~~ COMPARISON EDEhzhie # 
+# TODO: This one will be done for paper2 
 
 source('/mnt/data/Micheletti/NWT/posthocFunctions/compareAveragesInOutSHP.R')
 shp <- "https://drive.google.com/open?id=1GA7hGslGEE1DGIMsD4Ou9duesS-eGbyZ"
@@ -73,33 +74,23 @@ toUpload <- file.path("/mnt/data/Micheletti/NWT/outputs/30JUL19", "rasterEdehzie
 file.exists(toUpload)
 googledrive::drive_upload(toUpload, path = googledrive::as_id("1EmkeRatJdMjwxvJBQjYwLRF7jDrujy5G"))
 
-# ~~~~~~~~~~~~~~~~~BIRDS CHANGE TABLE
-
-# [ FIX ] Still need to test this function with shp == NULL!
-folderBirds <- "/mnt/data/Micheletti/NWT/outputs/09OCT19/LandR.CS_fS/run1/birdPredictions"
-source('/mnt/data/Micheletti/NWT/posthocFunctions/bootstrapPercentChanges.R') # INTERNAL FUNCTIONS TO BE PUT IN USEFUN!!! [ FIX ]
-shp <- "https://drive.google.com/open?id=1GA7hGslGEE1DGIMsD4Ou9duesS-eGbyZ"
-cacheFolder <- "/mnt/data/Micheletti/NWT/cache/"
-SpaDES.core::setPaths(cachePath = cacheFolder)
-boot <- bootstrapPercentChanges(folder = folderBirds, 
-                                    years = c(2011, 2091), sampleSize = 50, n = 2, shp = NULL) # FIGURE OUT WHY IT TAKES SO LONG TO SIMULATE!
-saveRDS(object = boot, file = file.path(folderBirds, "speciesChangeTables.rds"))
 # ~~~~~~~~~~~~~~~~~ CARIBOU POP GROWTH
+# BEING GENERATED IN THE MODULE!
+# plotCaribou <- plotCaribou(startTime = 2011,
+#                                currentTime = 2100,
+#                                endTime = 2100,
+#                                predictedCaribou = NWT$predictedCaribou,
+#                                yearSimulationStarts = 2001)
 
-plotCaribou <- plotCaribou(startTime = 2011,
-                               currentTime = 2100,
-                               endTime = 2100,
-                               predictedCaribou = NWT$predictedCaribou,
-                               yearSimulationStarts = 2001)
-
-# ~~~~~~~~~~~~~~~~~ CARIBOU
+# ~~~~~~~~~~~~~~~~~ CARIBOU RSF
 caribouArea1 <- prepInputs(url = "https://drive.google.com/open?id=1Qbt2pOvC8lGg25zhfMWcc3p6q3fZtBtO", 
                            destinationPath = tempdir(), filename2 = "caribouArea1")
-# 
+
 # corrModel2011 <- makeCorrelationModel(caribouRSFRas = raster::raster("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/caribouRSF/relativeSelectionTaigaPlains_Year2011.tif"), 
 #                                   birdDiversityRas = raster::stack("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/comMetrics/diverstiyIndices/diversityIndicesYear2011.tif"), 
 #                                   studyArea = caribouArea1)
 
+# TODO: This one will be done for paper4
 corrModel2100 <- makeCorrelationModel(caribouRSFRas = raster::raster("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/caribouRSF/relativeSelectionTaigaPlains_Year2100.tif"), 
                                   birdDiversityRas = raster::stack("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/comMetrics/diverstiyIndices/diversityIndicesYear2100.tif"), 
                                   studyArea = caribouArea1)
@@ -110,7 +101,7 @@ googledrive::drive_upload(file.path("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS
                           path = googledrive::as_id("130r_99kfhLIJE0Mz1FjyhiZnfM7s7-v6"))
 
 ### FOR EDEHZIE
-
+# TODO: This one will be done for paper4
 ede <- raster::raster("/mnt/data/Micheletti/NWT/outputs/edehzhieRaster.tif")
 
 corrModelEdeh2100 <- makeCorrelationModel(caribouRSFRas = raster::raster("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/caribouRSF/relativeSelectionTaigaPlains_Year2100.tif"), 
@@ -191,13 +182,29 @@ CS_2100 <- RSFplot(ras = predRS_CS$predictedPresenceProbability$Year2100$TaigaPl
 
 
 # ~~~~~~~~~~~~~~~~~ BIRDS
+# # ~~~~~~~~~~~~~~~~~BIRDS CHANGE TABLE
+
+# [ FIX ] Still need to test this function with shp == NULL!
+folderBirds <- "/mnt/data/Micheletti/NWT/outputs/09OCT19/LandR.CS_fS/run1/birdPredictions"
+source('/mnt/data/Micheletti/NWT/posthocFunctions/bootstrapPercentChanges.R') # INTERNAL FUNCTIONS TO BE PUT IN USEFUN!!! [ FIX ]
+shp <- "https://drive.google.com/open?id=1GA7hGslGEE1DGIMsD4Ou9duesS-eGbyZ"
+cacheFolder <- "/mnt/data/Micheletti/NWT/cache/"
+SpaDES.core::setPaths(cachePath = cacheFolder)
+boot <- bootstrapPercentChanges(folder = folderBirds, 
+                                years = c(2011, 2100), sampleSize = 50, n = 2, shp = NULL) # FIGURE OUT WHY IT TAKES SO LONG TO SIMULATE!
+saveRDS(object = boot, file = file.path(folderBirds, "speciesChangeTables.rds"))
+
+# ~~~~~~~~~~~~~~~~~ summaryBirds
+
 library("usefun")
+
 birdsFolder <- file.path(getwd(), CSfolder, 'birdPredictions/')
+
 birds2011 <- usefun::grepMulti(x = list.files(birdsFolder, 
                                       full.names = TRUE), patterns = c("predicted", "2011"))
-birds2091 <- usefun::grepMulti(x = list.files(birdsFolder, 
-                                      full.names = TRUE), patterns = c("predicted", "2091"))
-birds <- c(birds2011, birds2091)
+birds2100 <- usefun::grepMulti(x = list.files(birdsFolder, 
+                                      full.names = TRUE), patterns = c("predicted", "2100"))
+birds <- c(birds2011, birds2100)
 library("data.table")
 resultsBirdSpecies <- rbindlist(lapply(birds, function(ras){
   r <- raster::raster(ras)
@@ -212,21 +219,21 @@ resultsBirdSpecies <- rbindlist(lapply(birds, function(ras){
 write.csv(x = resultsBirdSpecies, file = file.path(birdsFolder, "summaryBirds.csv"))
 rm(resultsBirdSpecies)
 
-species <- c("RUBL", "OSFL", "CAWA")
+species <- c("RUBL", "OSFL", "CAWA", "BBWA", "WEWP")
 
 createBirdsGIFFromList(pathData = file.path("/mnt/data/Micheletti/NWT/outputs/18JUN19_CS_SCFM/birdPredictions/"), 
                        species = species, mainFileName = "CSpredicted", ysrName = c(seq(2011, 2100, by = 10), 2100),
                        version = "V6", uploadFiles = TRUE, whereToReport = "BCR6_NWT")
 
-WhichToUp <- c("/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.0_30MAR19.png",
-            "/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.50_30MAR19.png",
-            "/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.100_30MAR19.png",
-            "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.0_30MAR19.png",
-            "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.50_30MAR19.png",
-            "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.100_30MAR19.png")
-
-  lapply(WhichToUp, function(each){
-    googledrive::drive_upload(each,
-                              path = as_id("1lhhIr_865VZI05mhiQ91iN8MfDGn5PV7"))
-  })
+# WhichToUp <- c("/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.0_30MAR19.png",
+#             "/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.50_30MAR19.png",
+#             "/mnt/data/Micheletti/NWT/outputs/PredictedAMREYear.100_30MAR19.png",
+#             "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.0_30MAR19.png",
+#             "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.50_30MAR19.png",
+#             "/mnt/data/Micheletti/NWT/outputs/PredictedOVENYear.100_30MAR19.png")
+# 
+#   lapply(WhichToUp, function(each){
+#     googledrive::drive_upload(each,
+#                               path = as_id("1lhhIr_865VZI05mhiQ91iN8MfDGn5PV7"))
+#   })
 

6badf36543373df384d8c6afede9d40ee2efcb83
Tati Micheletti <33500569+tati-micheletti@users.noreply.github.com>
2019-12-09 01:31:22 +0100

    debugging

--- modules/posthocLandR/posthocLandR.R
+++ modules/posthocLandR/posthocLandR.R
@@ -67,7 +67,9 @@ doEvent.posthocLandR = function(sim, eventTime, eventType) {
       sim <- scheduleEvent(sim, start(sim), "posthocLandR", "createPlots")
     },
     createPlots = {
-      sim$allPlots <- future_lapply(names(sim$resultsFolders), function(typeSim){  # [ FIX ] still need to make use of the overwrite! # future.apply::future_lapply
+      print("Debug in createPlots event")
+      browser()
+      sim$allPlots <- lapply(names(sim$resultsFolders), function(typeSim){  # [ FIX ] still need to make use of the overwrite! # future.apply::future_lapply
         sim$LeadingVegetationType[[typeSim]] <- plotLeadingVegetationType(dataPath = sim$resultsFolders[[typeSim]], typeSim = typeSim, saveRAS = P(sim)$saveRAS,
                                                                           overwrite = P(sim)$overwriteLeadingMap)
         sim$vegetationBiomassMap[[typeSim]] <- plotVegetationBiomass(dataPath = sim$resultsFolders[[typeSim]], typeSim = typeSim,

b4ac2a1b34c124604e1c18cec6d87eba54b49e10
Tati Micheletti <tati.micheletti@gmail.com>
2019-12-12 15:33:54 +0100

    debugging fun makeDiffAnalysis

--- posthocFunctions/makeDiffAnalysis.R
+++ posthocFunctions/makeDiffAnalysis.R
@@ -28,7 +28,7 @@ makeDiffAnalysis <- function(resultsFolder = file.path(getwd(), "outputs/23OCT19
     stop("Comparisons need to be a named list of what you are making the differences")
   if (length(comparisons[[names(comparisons)]])>2)
     stop("Comparisons can only be made for 2 groups for now")
-  
+  browser()
   allRasters <- unlist(future_lapply(Bird, function(bird){
     birdRasters <- future_lapply(Year, function(year){ #future_lapply
       birdYearRasters <- lapply(Scenario, function(scenario){

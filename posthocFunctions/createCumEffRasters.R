161958e47c97e885ebbc012f5e25db08e3b90393
Tati Micheletti <tati.micheletti@gmail.com>
2020-01-28 20:09:57 -0800

    generic cummRas

--- posthocFunctions/createCumEffRasters.R
+++ posthocFunctions/createCumEffRasters.R
@@ -1,6 +1,6 @@
-createCumEffRasters <- function(birds = c("CAWA", "OSFL", "RUBL"),
-                                rasFolder = "/mnt/data/Micheletti/NWT/outputs/06DEC19/effectsRasters",
-                                googlefolderID = "1lnM3Va3UklcGy_Swlc7AY1Ww7nImPYLM"){
+createCumEffRasters <- function(species = c("CAWA", "OSFL", "RUBL"),
+                                rasFolder = "/mnt/data/Micheletti/NWT/outputs/PAPER/effectsRasters",
+                                googlefolderID = NULL){
   library(raster)
   library(googledrive)
   library(future)
@@ -8,24 +8,26 @@ createCumEffRasters <- function(birds = c("CAWA", "OSFL", "RUBL"),
   library(usefun)
   plan("multicore")
   
-  future_lapply(birds, function(BIRD){
-    birdsPaths <- grepMulti(list.files(rasFolder, full.names = TRUE), 
-                            patterns = c("cumulativeEffect", "_abs_", BIRD, "delta.tif"))
-    BIRDS <- stack(lapply(birdsPaths, function(pths){
+  future_lapply(species, function(SP){
+    birdsPaths <- grepMulti(list.files(rasFolder, full.names = TRUE),
+                            patterns = c("cumulativeEffect", SP, "delta.tif"))
+    SP <- stack(lapply(birdsPaths, function(pths){
       B <- raster(pths)
       B[] <- B[]
       return(B)
     })
     )
-    averageBIRD <- calc(BIRDS, fun = mean)
-    sdBIRD <- calc(BIRDS, fun = sd)
-    aveName <- file.path(rasFolder, paste0("averageDelta_abs_", BIRD, ".tif"))
-    sdName <- file.path(rasFolder, paste0("sdDelta_abs_", BIRD, ".tif"))
-    writeRaster(averageBIRD, filename = aveName, format = "GTiff")
-    writeRaster(sdBIRD, filename = sdName, format = "GTiff")
-    lapply(c(aveName, sdName), function(ras){
-      drive_upload(ras, as_id(googlefolderID))
-    })
+    averageSP <- calc(SP, fun = mean)
+    sdSP <- calc(SP, fun = sd)
+    aveName <- file.path(rasFolder, paste0("averageDelta_abs_", SP, ".tif"))
+    sdName <- file.path(rasFolder, paste0("sdDelta_abs_", SP, ".tif"))
+    writeRaster(averageSP, filename = aveName, format = "GTiff")
+    writeRaster(sdSP, filename = sdName, format = "GTiff")
+    if (!is.null(googlefolderID)){
+      lapply(c(aveName, sdName), function(ras){
+        drive_upload(ras, as_id(googlefolderID))
+      })
+    }
   })
-  print(paste0("Rasters created for ", birds))
+  print(paste0("Rasters created for ", species))
 }

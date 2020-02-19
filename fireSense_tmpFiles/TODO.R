dd48f4f47abd808f9d827f34eddd2d2c7709f6ab
Tati Micheletti <tati.micheletti@gmail.com>
2020-02-18 20:48:42 -0800

    files for fixing fireSense

---
+++ fireSense_tmpFiles/TODO.R
@@ -0,0 +1,7 @@
+# Steps for fireSense
+# 0. Prepare the formula and layers (the same for all three components): need for SpreadFit and will be useful later for the WB project
+# ==> This should be a module, and named `fireSense_DataPrep`
+# 1. Try to run IgnitionFitted + EscapeFitted (objects from Fit modules) with SpreadFit
+# 2. Try to run 1 + predict of all
+# 3. Try to run IgnitionFit, EscapeFit, SpreadFit + predicts
+# 4. Try to run 3 + SizeFit + SizePredict 

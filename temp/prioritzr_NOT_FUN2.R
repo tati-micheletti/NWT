4f63bb49f8781eae190197aecafa325f27c6df97
Tati Micheletti <tati.micheletti@gmail.com>
2020-03-06 14:35:14 -0800

    minor

--- temp/prioritzr_NOT_FUN2.R
+++ temp/prioritzr_NOT_FUN2.R
@@ -27,7 +27,7 @@ p <- add_min_set_objective(p)
 # (Ball et al. 2009). For example, we can add a minimum set objective to a problem using the 
 # following code.
 
-# 3. Rules/Targets:
+# Rules/Targets:
 # create a problem with targets which specify that we need 10 % of the habitat
 # for the first feature, 15 % for the second feature, 20 % for the third feature
 # 25 % for the fourth feature and 30 % of the habitat for the fifth feature
@@ -37,11 +37,11 @@ p <- add_relative_targets(p, targets)
 
 # 4. Constraints:
 p <- add_locked_in_constraints(p, edehzhie)#: raster with existing conservation units
-# add_neighbor_constraints: guarantee a minimum size for each protected area
-# add_contiguity_constraints: make sure all units are contiguous
-# add_feature_contiguity_constraints: make sure all units are contiguous for each feature!
-# add_locked_out_constraints(raster): can be used to lock areas out of the solution. Potentially for 
-# scenario planning -- i.e. mining?
+"add_neighbor_constraints: guarantee a minimum size for each protected area"
+"add_contiguity_constraints: make sure all units are contiguous"
+"add_feature_contiguity_constraints: make sure all units are contiguous for each feature!"
+"add_locked_out_constraints(raster): can be used to lock areas out of the solution. Potentially for"
+"scenario planning -- i.e. mining"
 
 # 5. Penalties
 # add_boundary_penalties(penalty = 0.01):  penalize solutions that are excessively fragmented. 
@@ -54,8 +54,8 @@ p <- add_locked_in_constraints(p, edehzhie)#: raster with existing conservation 
 # will create a matrix showing the average strength of connectivity between
 # adjacent planning units using the data in the fifth layer of sim_features
 p <- add_boundary_penalties(p, penalty = 5,
-                       data = connectivity_matrix(planningUnit,
-                                                  tekMap))
+                       data = connectivity_matrix(planningUnitRaster,
+                                                  TEKRaster))
 # This can be used for TEK informed maps! i.e. where do caribou cross?
 
 # 6. Decisions
@@ -67,6 +67,6 @@ p <- add_pool_portfolio(p, method = 2, number_solutions = 10)
 # Solver: 
 p <- add_gurobi_solver(p, gap = 0)
 
-conservationUnits <- solve(p) # Outputs a raster if original is a raster.
+conservationUnits <- solve(p) # Outputs a raster if original is a raster. # CHECK THIS FOR PARALLEL
 
 # Should be a table, though

00918f35d8382ac80c30fcf0bf5d497f62ab30a4
Tati Micheletti <tati.micheletti@gmail.com>
2020-03-24 11:08:50 -0700

    Moved fun getFirePolygons_NFDB from fireSense to NWT

---
+++ functions/getFirePolygons_NFDB.R
@@ -0,0 +1,35 @@
+getFirePolygons_NFDB <- function(studyArea = NULL, rasterToMatch = NULL,
+                                 redownloadIn = 1, # Time in YEARS that we tolerate the data to be "old" i.e. 0.5 would mean "redownload data older than 6 months"
+                                 NFDB_polyPath # Can't be NULL. Needs to be an existing location for the fire points
+                                 ){
+    check <- Checksums(NFDB_polyPath, checksumFile = file.path(NFDB_pointPath, "CHECKSUMS.txt"), write = TRUE)
+    whRowIsShp <- grep("NFDB_poly.*shp$", check$expectedFile)
+    whIsOK <- which(check$result[whRowIsShp] == "OK")
+    needNewDownload <- TRUE
+    if (any(whIsOK)) {
+      filesToCheck <- tools::file_path_sans_ext(unlist(lapply(check[whRowIsShp[whIsOK], "expectedFile"], as.character)))
+      dateOfFile <- substr(x = filesToCheck, start = nchar(filesToCheck) - 8 + 
+                             1, nchar(filesToCheck))
+      if (any((as.Date(dateOfFile, format = "%Y%m%d") + dyear(redownloadIn)) > Sys.Date())) {
+        # can change dyear(...) to whatever... e.g., dyear(0.5) would be 6 months
+        needNewDownload <- FALSE
+      }
+    }
+    if (needNewDownload) {
+      print("downloading NFDB")# put prepInputs here
+      firePolys <- Cache(prepInputs, url = url,
+                          studyArea = studyArea, fun = "shapefile",
+                          destination = NFDB_pointPath, useCache = "overwrite",
+                          useSAcrs = TRUE, omitArgs = c("NFDB_pointPath", "overwrite"))
+    } else {
+      NFDBs <- grep(list.files(NFDB_pointPath), pattern = "^NFDB", value = TRUE)
+      shps <- grep(list.files(NFDB_pointPath), pattern = ".shp$", value = TRUE)
+      aFile <- NFDBs[NFDBs %in% shps][1] #in case there are multiple files
+      firePolys <- Cache(shapefile, file.path(NFDB_pointPath, aFile))
+      firePolys <- Cache(postProcess, x = firePolys,
+                          studyArea = studyArea, filename2 = NULL,
+                          rasterToMatch = rasterToMatch,
+                          userTags = c("cacheTags", "NFDB"))
+    }
+    return(firePolys)
+  }

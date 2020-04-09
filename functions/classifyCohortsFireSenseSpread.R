a680010fe3562d5566cafd9317495ed8b593e194
Tati Micheletti <tati.micheletti@gmail.com>
2020-04-08 22:41:46 -0700

    Fixing class5

--- functions/classifyCohortsFireSenseSpread.R
+++ functions/classifyCohortsFireSenseSpread.R
@@ -1,9 +1,9 @@
-classifyCohortsFireSenseSpread <- function(cohortData, yearCohort){
+classifyCohortsFireSenseSpread <- function(cohortData, yearCohort, pixelGroupMap){
   spCode <- c('Pice_Mar', 'Pice_Gla', 'Lari_Lar', 'Betu_Pap', 'Popu_Tre', 'Pinu_Ban') # TODO Make it flexible and into a function!
   reclassTable <- data.table(speciesCode = spCode, burnClass = c("class3", "class3", "class3", "class2", "class2", "class4"))
   cohortData <- merge(cohortData, reclassTable, by = "speciesCode", all.x = TRUE)
   cohortData[age < 15, burnClass := "class1"]
-  cohortData[is.na(B), burnClass := "class5"]
+  # cohortData[is.na(B), burnClass := "class5"] # Potentially not happening here.... We should not have is.na(B)
   #Assertion
   testthat::expect_true(NROW(cohortData[is.na(totalBiomass) & burnClass != "class5", ])==0)
   testthat::expect_true(NROW(cohortData[!is.na(totalBiomass) & burnClass == "class5", ])==0)
@@ -21,11 +21,33 @@ classifyCohortsFireSenseSpread <- function(cohortData, yearCohort){
   
   classList <- lapply(paste0("class", 1:4), function(cls){
     cohortDataub <- cohortData[burnClass == cls, ]
-    ras <- SpaDES.tools::rasterizeReduced(reduced = cohortDataub, fullRaster = pixelGroupMap2001, 
-                                          newRasterCols = "propBurnClassFire", mapcode = "pixelGroup")
+    ras <- SpaDES.tools::rasterizeReduced(reduced = cohortDataub, fullRaster = pixelGroupMap, 
+                                          newRasterCols = "propBurnClassFire", 
+                                          mapcode = "pixelGroup")
     names(ras) <- paste0(cls, "_", yearCohort)
     return(ras)
   })
-  names(classList) <- paste0("class", 1:4)
+  
+  ####################### Prep Layers: Identify non-forested pixels (non-ice/water/rocks) as class5
+  # Pixels that are *NOT* NA in the RTM when this has been NA'ed for water, ice, and rocks, and 
+  # ARE NA in the pixelGroupMap are the pixels that are class5
+browser()
+  class5ras <- raster(pixelGroupMap)
+  class5 <- pixelGroupMap[is.na(pixelGroupMap) & !is.na(nonFlammable)]
+  class5ras[class5pix] <- 1
+
+  classList <- c(classList, list(class5ras))
+  # # To create the class5, I need to do 1-sum(class1:4)
+  # class5_2001 <- calc(x = stack(classList2001), fun = sum, na.rm = TRUE)
+  # classList2001[["class5"]] <- 1 - class5_2001
+  # names(classList2001[["class5"]]) <- "class5_2001"
+  # class5_2011 <- calc(x = stack(classList2011), fun = sum, na.rm = TRUE)
+  # classList2011[["class5"]] <- 1 - class5_2011
+  # names(classList2011[["class5"]]) <- "class5_2011"
+  #
+  # # CHECK CLASS5 is only 1 or 0
+  # #TODO
+
+  names(classList) <- paste0("class", 1:5)
   return(classList)
 }

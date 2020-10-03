42ec1452e3aefc07fca90c0611e8eade4534b48d
Tati Micheletti <tati.micheletti@gmail.com>
2020-10-03 07:32:13 +0000

    Working. Checkpoint

--- 6_posthocAnalysis.R
+++ 6_posthocAnalysis.R
@@ -13,14 +13,16 @@ SpaDES.core::setPaths(cachePath = posthocCache,
 #########################################################
 ##                    B I R D S                        ##
 #########################################################
-
+if (!exists("runPosthocBirds")) runPosthocBirds <- FALSE
 if (runPosthocBirds){
   if (all(!exists("originalDateAnalysis"),
           !(runLandR))) originalDateAnalysis <- "14AUG20" # Default if not provided
   if (!exists("birdModelVersion")) birdModelVersion <- c("4", "6a") # Default if not provided 
-  comparisons <- list(climateChange = 
-                        c("LandR_SCFM", 
-                          "LandR.CS_fS")) # Not sure this works...
+  comparisons <- list(climate = c("V6a", "V4"),
+                      vegetation = c("LandR.CS_", "LandR_"),
+                      fire = c("fS", "SCFM"))
+  # IMPORTANT: In the comparisons, always the climate sensitives need to come first!! This is
+  # expected by the internal functions
 }
 # Structure of the results from the simulations
 # originalDateAnalysis --> comparison --> runs --> birdModels
@@ -69,73 +71,113 @@ SpeciesNoClim <- unique(sort(substrBoth(strng = substrBoth(strng = tools::file_p
 
 testthat::expect_true(all(Species %in% SpeciesNoClim))
 
+
 #############################
-# runs <- paste0("run", 1:10) # Uncomment when testing is complete
-runs <- paste0("run", 1:2)
+
+# Shapefile to summarize
+urlGNWTEcoregions <- "https://drive.google.com/file/d/1Cf--HP0Zq1_VziN_BLN7erBFHnzxvUsy/view?usp=sharing"
+shpSummary <- prepInputs(url = urlGNWTEcoregions,
+                        destinationPath = Paths$inputPath,
+                        studyArea = studyArea,
+                        filename2 = NULL,
+                        userTags = c("objectName:shpSummary", stepCacheTag), 
+                        omitArgs = c("destinationPath"))
+#############################
+runs <- paste0("run", 1:10)
+TIME <- 1
 
 parameters <- list(
   posthocBirdsNWT = list(
-    "simulationStamp" = "Testing2", #toupper(format(Sys.time(), "%d%b%y"))
-    # "species" = Species, # To be uncommented when the tests are over
-    "species" = c("HOLA", "PAWA"), # To be removed when the tests are over
-    # "years" = c(seq(2011, 2091, by = 20), 2100), # To be uncommented when the tests are over
-    "years" = c(2011, 2051, 2100), # To be removed when the tests are over
+    "simulationStamp" = toupper(format(Sys.time(), "%d%b%y")),
+    "species" = Species, # To be uncommented when the tests are over
+    "years" = c(seq(2011, 2091, by = 20), 2100), # To be uncommented when the tests are over
     "relativeDelta" = FALSE,
     "runs" = runs,
-    "useFuture" = FALSE,
-    # "patternsUsedForGrouping" = paste0("run", 1:10), # To be uncommented when the tests are over
+    "useFuture" = TRUE,
     # "nBootReps" = 50 # To be uncommented when the tests are over
     # "uploadPlots" = TRUE # To be uncommented when the tests are over
-    "comparisonID" = "climateChangeEffects",
     "birdModels" = c("V4", "V6a")
     # , overwriteBootstrap = TRUE
     )
   )
 
-dataFolder <- lapply(comparisons[['climateChange']], function(scenario){
-  dataFolderRuns <- lapply(parameters[['posthocBirdsNWT']][['runs']], function(run){
-                             birdModel <- ifelse(scenario == "LandR.CS_fS", 
-                                                 "birdPredictionsV6a", 
-                                                 "birdPredictionsV4")
-                             pth <- file.path(dirname(Paths$outputPath),
-                                              originalDateAnalysis,
-                                              scenario,
-                                              run,
-                                              birdModel)
-                             return(pth)
+# dataFolder <- lapply(comparisons[['climateChange']], function(scenario){
+#   dataFolderRuns <- lapply(parameters[['posthocBirdsNWT']][['runs']], function(run){
+#                              birdModel <- ifelse(scenario == "LandR.CS_fS",
+#                                                  "birdPredictionsV6a", 
+#                                                  "birdPredictionsV4")
+#                              pth <- file.path(dirname(Paths$outputPath),
+#                                               originalDateAnalysis,
+#                                               scenario,
+#                                               run,
+#                                               birdModel)
+#                              return(pth)
+#   })
+#   names(dataFolderRuns) <- parameters[['posthocBirdsNWT']][['runs']]
+#   return(dataFolderRuns)
+# })
+# names(dataFolder) <- comparisons[['climateChange']]
+
+vegetationFireModels <- expand.grid(vegetation = c("LandR_", "LandR.CS_"), 
+                                    fire = c("fS", "SCFM"))
+vegetationFireModels <- paste0(vegetationFireModels$vegetation, vegetationFireModels$fire)
+
+dataFolder <- lapply(vegetationFireModels, function(scenario){
+    dataFolderRuns <- lapply(c("V4", "V6a"), function(birdModel){
+      dataFolderRuns <- lapply(parameters[['posthocBirdsNWT']][['runs']], function(run){
+        pth <- file.path(dirname(Paths$outputPath),
+                          originalDateAnalysis,
+                          scenario,
+                          run,
+                          paste0("birdPredictions", birdModel))
+      return(pth)
+    })
+    names(dataFolderRuns) <- parameters[['posthocBirdsNWT']][['runs']]
+    return(dataFolderRuns)
   })
-  names(dataFolderRuns) <- parameters[['posthocBirdsNWT']][['runs']]
+  names(dataFolderRuns) <- c("V4", "V6a")
   return(dataFolderRuns)
 })
-names(dataFolder) <- comparisons[['climateChange']]
+names(dataFolder) <- vegetationFireModels
 
-# Loading the bird rasters I want for comparison. As I am not interested in the predictions of 
-# V6a with LandR_SCFM, I need to do this. If I was interested in that prediction, I would just 
-# leave the birdRasters object as NULL
-
-birdRasters <- getBirdPredictedRasters(parameters = parameters, 
-                                       predictedRastersFolder = file.path(dirname(Paths$outputPath),
-                                                                          originalDateAnalysis))
+# # Loading the bird rasters I want for comparison. If I am not interested in the predictions of 
+# # V6a with LandR_SCFM, I need to do this. If I was interested in that prediction, I would just 
+# # leave the birdRasters object as NULL
+# 
+# birdRasters <- getBirdPredictedRasters(parameters = parameters, 
+#                                        predictedRastersFolder = file.path(dirname(Paths$outputPath),
+#                                                                           originalDateAnalysis))
 
 objects <- list(
-  # "googleFolders" = "" # Pass where to upload the files to when finished testing
   "dataFolder" = dataFolder,
-  "studyArea" = studyArea,
   "rasterToMatch" = rasterToMatch,
   "comparisons" = comparisons,
-  "birdRasters" = birdRasters,
+  "studyAreaPosthoc" = shpSummary,
   "predictedRastersFolder" = file.path(dirname(Paths$outputPath),
                                        originalDateAnalysis)
 )
 
+outputsBirds <- data.frame(
+  objectName = c("pixelsSummaries",
+                     "differenceRasters",
+                     "colonizationRasters",
+                     "deltaRasters",
+                     "significantChanges",
+                     "averageTimePlot",
+                     "cummEffRas"),
+  saveTime = TIME)
+  
+  
 # Maybe we should apply a treatment to the predicted rasters 
 # to convert "virtual zeros" into zeros...
 
-posthocBirdsAnalysis <- simInitAndSpades(times = list(start = 1, end = 1),
-                                         params = parameters,
+posthocBirdsAnalysis <- simInitAndSpades(times = list(start = TIME, end = TIME),
+                                         params = parameters, 
+                                         outputs = outputsBirds,
                                          modules = list("posthocBirdsNWT"),
                                          objects = objects,
                                          paths = Paths,
                                          loadOrder = "posthocBirdsNWT",
                                          debug = 1
                                          )
+  

b0cb2a57bf26a91302daff0dd29285c592ba2087
Tati Micheletti <tati.micheletti@gmail.com>
2021-02-18 19:40:43 +0000

    Caribou MS maps function -- ready to rol

--- posthocFunctions/makeCaribouRSFAverageMap.R
+++ posthocFunctions/makeCaribouRSFAverageMap.R
@@ -90,22 +90,12 @@ makeCaribouRSFAverageMap <- function(resultsFolder,
                        averageRSF = aveRSF)
     }))
     
-    # Make maps for these averages
-    pal <- c("#c2473d",
-             "#d96b52",
-             "#e1936b",
-             "#f4bd81",
-             "#f8e8b0",
-             "#ecedc2",
-             "#bec3c7",
-             "#95a3ca",
-             "#6c7ed0",
-             "#3260c6") # Handmade to match DeMars 2019
-    
+  
     library("lattice")
     library("rasterVis")
     library("viridis")
     library("maptools")
+    library("colorspace")
     
     pathSHP <- file.path(outputsPath, "RSFshp.shp")
     if (!file.exists(pathSHP)){
@@ -114,14 +104,18 @@ makeCaribouRSFAverageMap <- function(resultsFolder,
     }
   shpLoaded <- maptools::readShapeLines(pathSHP)
 
-    # Add shp to levelplot
   bothMaps <- rbindlist(lapply(names(cmStk), function(layName){
     
     pngPath <- file.path(outputsPath, paste0(layName, "_", cm,".png"))
     
     if (layName == "averageDifference"){
+      # round these up to the integer
+      cmStk[[layName]] <- round(cmStk[[layName]], 0)
       cmStk[[layName]] <- ratify(cmStk[[layName]])
-      Pal <- pal
+      Pal <- c("#7d0125", "#a90d00", "#d22d00", "#eb5500", "#f18000", "#f6a400", "#f7c252",
+               "#fbdc8a", "#fff1b2", "#ffffc9", "#cfeea8", "#a6dea1", "#7fcc9e", "#57ba9d", 
+               "#32a699", "#109196", "#047c8d", "#1b6685", "#2e4f7a")
+      att <- "ID"
       typePlot <- "Average"
     } else {
       Pal <- viridis_pal(option = "D")(10) 
@@ -131,9 +125,8 @@ makeCaribouRSFAverageMap <- function(resultsFolder,
     png(filename = pngPath,
         width = 21, height = 29,
         units = "cm", res = 300)
-    browser()
     print(rasterVis::levelplot(cmStk[[layName]],
-                               sub = paste0(typePlot, " RSF for ", cm),
+                               sub = paste0(typePlot, " RSF difference for ", cm),
                                att = att,
                                margin = FALSE,
                                maxpixels = 6e6,
@@ -148,7 +141,7 @@ makeCaribouRSFAverageMap <- function(resultsFolder,
                                  strip.background = list(col = 'transparent'),
                                  axis.line = list(col = 'transparent')),
                                scales = list(draw = FALSE),
-                               col.regions = Pal, #pals::kovesi.rainbow(nlev), #viridis_pal(option = "D")(nlev),
+                               col.regions = Pal,
                                par.strip.text = list(cex = 0.8,
                                                      lines = 1,
                                                      col = "black"),
@@ -169,14 +162,104 @@ makeCaribouRSFAverageMap <- function(resultsFolder,
                meanRSFPoly = calcMeanRSF))
 
   })
+  names(booMaps) <- climateModels
+  allRepsMaps <- unlist(x = lapply(booMaps, `[[`, "diffRas"),
+                        recursive = FALSE)
   
-  # booMaps$mapsFilepath # To return
+  ## Average and sd of all maps
+  meanDiffName <- file.path(outputsPath,
+                            paste0("averageRSFdiff"))
+  if (any(overwriteCalc,
+          !file.exists(paste0(meanDiffName, ".tif")))){
+    message(paste0("Calculating average for all climate scenarios"))
+    averageCM <- calc(stack(allRepsMaps), fun = mean, 
+                      na.rm = TRUE, overwrite = TRUE,
+                      filename = meanDiffName,
+                      format = "GTiff")
+  } else {
+    averageCM <- raster(paste0(meanDiffName, ".tif"))
+  }
   
-  diffRas <- unlist(booMaps$diffRas, recursive = FALSE) # Probably unlist one level
+  # Values need to go from -9 to 9 (RSF is only from 1 to 10)
+  sdDiffName <- file.path(outputsPath,
+                          paste0("sdRSFdiff"))
+  if (any(overwriteCalc,
+          !file.exists(paste0(sdDiffName, ".tif")))){
+    message(paste0("Calculating SD for all climate scenarios"))
+    sdCM <- calc(stack(allRepsMaps), fun = sd, 
+                 na.rm = TRUE, overwrite = TRUE,
+                 filename = sdDiffName, 
+                 format = "GTiff") 
+  } else {
+    sdCM <- raster(paste0(sdDiffName, ".tif"))
+  }
+  names(averageCM) <- "averageDifference"
+  names(sdCM) <- "sdDifference"
+  cmStk <- stack(averageCM, sdCM)
   
-  meanRSFPoly <- rbindlist(booMaps$meanRSFPoly) # Table with the mean by poly
-  browser()
-  # Average those and make SD. Save (Appendix)
-  # Calculate the diff per polygon
-  # Make maps, save png
+  # MAKE FINAL MAPS!
+  pathSHP <- file.path(outputsPath, "RSFshp.shp")
+  shpLoaded <- maptools::readShapeLines(pathSHP)
+  
+  bothMapsAllCS <- rbindlist(lapply(X = names(cmStk), cmStk = cmStk, 
+                               FUN = function(layName, cmStk){
+    
+    pngPath <- file.path(outputsPath, paste0(layName, "_allScenarios.png"))
+    
+    if (layName == "averageDifference"){
+      # round these up to the integer
+      cmStk[[layName]] <- round(cmStk[[layName]], 0)
+      cmStk[[layName]] <- ratify(cmStk[[layName]])
+      Pal <- c("#7d0125", "#a90d00", "#d22d00", "#eb5500", "#f18000", "#f6a400", "#f7c252",
+               "#fbdc8a", "#fff1b2", "#ffffc9", "#cfeea8", "#a6dea1", "#7fcc9e", "#57ba9d", 
+               "#32a699", "#109196", "#047c8d", "#1b6685", "#2e4f7a")
+      att <- "ID"
+      typePlot <- "Average"
+    } else {
+      Pal <- viridis_pal(option = "D")(10) 
+      att <- NULL
+      typePlot <- "SD"
+    }
+    png(filename = pngPath,
+        width = 21, height = 29,
+        units = "cm", res = 300)
+    
+    print(rasterVis::levelplot(cmStk[[layName]],
+                               sub = paste0(typePlot, " RSF difference for all climate scenarios"),
+                               att = att,
+                               margin = FALSE,
+                               maxpixels = 6e6,
+                               colorkey = list(
+                                 space = 'bottom',
+                                 # at = -9:9,
+                                 axis.line = list(col = 'black'),
+                                 width = 0.75
+                               ),
+                               par.settings = list(
+                                 strip.border = list(col = 'transparent'),
+                                 strip.background = list(col = 'transparent'),
+                                 axis.line = list(col = 'transparent')),
+                               scales = list(draw = FALSE),
+                               col.regions = Pal,
+                               par.strip.text = list(cex = 0.8,
+                                                     lines = 1,
+                                                     col = "black"),
+                               panel = function(...){
+                                 lattice::panel.levelplot.raster(...)
+                                 sp::sp.polygons(shpLoaded, fill = 'black', lwd = 1)
+                               }))
+    
+    dev.off()
+    DTs <- data.table(climateModel = "allScenarios",
+                      fileType = typePlot, 
+                      fileLocation = pngPath)
+    return(DTs) # Return the location of the file 
+  }))
+  
+  allRepsFilepath <- rbindlist(lapply(booMaps, `[[`, "mapsFilepath")) 
+  finalMapsPath <- rbindlist(list(allRepsFilepath, bothMapsAllCS))
+  meanRSFPoly <- rbindlist(lapply(booMaps, `[[`, "meanRSFPoly"))
+  
+  return(list(meanRSFPoly = meanRSFPoly,
+              mapsFilePaths = finalMapsPath))
 }

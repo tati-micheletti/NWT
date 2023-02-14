ce3c1281b22a7446366b2ec1eaf00f19206413a1
Tati Micheletti <tati.micheletti@gmail.com>
2023-02-14 22:52:54 +0100

    Added posthoc fun for caribou

--- posthocFunctions/redoingBooPlotsWithReadyTable.R
+++ posthocFunctions/redoingBooPlotsWithReadyTable.R
@@ -3,12 +3,26 @@
 library("Require")
 Require("reproducible")
 Require("data.table")
+Require("ggplot2")
 
 urlFinalTable <- "https://drive.google.com/file/d/1ClbbSIeos2n4LugtBaSa9B2tqt4x_MiY/view?usp=sharing"
 pathOutputs <- "caribouPlots/"
 booTable <- prepInputs(url = urlFinalTable,
                        destinationPath = pathOutputs,
                        fun = "data.table::fread")
+# Clean up table
+booTable <- booTable[, V1 := NULL, ]
+booTable <- unique(booTable)
+
+# Checking normality of the results
+histPlots <- ggplot2::ggplot(data = booTable[femSurvMod_recrMod == "Johnson_M1_National::Johnson_M4_National" &
+                                               Year != 2011], 
+                             mapping = aes(x = annualLambda)) +
+  geom_histogram(binwidth = 0.001) +
+  # geom_density(alpha = .3, fill = "#FF6666") +
+  facet_grid(Polygon ~ Year, scales = 'free_x')
+histPlots
+
 
 source("posthocFunctions/plotCaribouPopGrowthMSSD.R")
 plotCaribou <- plotCaribouPopGrowthMSSD(whichPolys = c("Bistcho", "Maxhamish", "Yates"),
